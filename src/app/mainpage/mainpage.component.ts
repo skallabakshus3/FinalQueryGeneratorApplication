0fea4c13986ea2923054e66bba7a8a07e87c32fb
Shaik <allabakshu.shaik@capgemini.com>
2019-11-19 16:00:02 +0530

    third commit

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -7,13 +7,13 @@ import { Validators, FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./mainpage.component.css']
 })
 export class MainpageComponent implements OnInit {
- 
+
   @Output() addComponentClick = new EventEmitter();
   registerForm: FormGroup;
   submitted = false;
 
   Table: any = ['INPUT_VALIDATE_EXPRESSION', 'LOOKUP', 'LOOKUP_LIST']
-  toaster: any;
+  query: any;
   registerForm2: FormGroup;
   registerForm1: FormGroup;
 
@@ -23,32 +23,33 @@ export class MainpageComponent implements OnInit {
   // Method called by Angular once it has created the component
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      rEQFORM: ['', Validators.required],
-      oBJ_ID: ['', Validators.required],
-      oBJ_FIELD_ID: ['', Validators.required],
-      sUBGROUP_ID: ['', Validators.required],
+      rEQFORM: ['', [Validators.required, Validators.minLength(6)]],
+      oBJ_ID: ['', [Validators.required, Validators.minLength(6)]],
+      oBJ_FIELD_ID: ['', [Validators.required, Validators.minLength(6)]],
+      sUBGROUP_ID: ['', [Validators.required, Validators.minLength(6)]],
       oBJ_FIELD_TYPE_ID: ['', Validators.required],
       rEQUIREDTEXT: ['', Validators.required],
       dISPLAY_NAME: ['', [Validators.required, Validators.minLength(6)]],
       iSKEYMEMBER: ['', Validators.required],
 
       vALIDATION_EXPRESSION: ['', Validators.required],
-      vALIDATION_HELP_TEXT: ['', [Validators.required, Validators.minLength(6)]],
+      vALIDATION_HELP_TEXT: ['', Validators.required],
       vALIDATION_TOOL_TIP: ['', Validators.required],
 
-      // vALUE_LIST: ['', Validators.required],
+      vALUE_LIST: ['', Validators.required],
 
-
+      lOOKUP_TABLE_NAME: ['', Validators.required],
+      lOOKUP_TABLE_KEY: ['', Validators.required],
+      lOOKUP_TABLE_DESCRIPTION: ['', Validators.required],
+      lOOKUP_TABLE_FILTER: ['', Validators.required],
+      cASCADE: ['', Validators.required]
     });
 
-   
-   
-    
   }
 
   //getting form controls
   get f() { return this.registerForm.controls; }
- 
+
 
   //on submit form method
   onSubmit() {
@@ -58,25 +59,21 @@ export class MainpageComponent implements OnInit {
     if (this.registerForm.invalid) {
       return;
     }
-     //alert('SUCCESS!! :-)\n\n' + JSON.stringify('Copy Your Script'))
+    //alert('SUCCESS!! :-)\n\n' + JSON.stringify('Copy Your Script'))
   }
 
   //resetting form
   resetForm() {
     this.registerForm.reset();
   }
-  
 
   //copy method used to copy query in text area and save to clipboard
   copyToClipboard(element) {
     element.select();
     document.execCommand('copy');
-    this.toaster('success', 'Success!', 'Link copied to clipboard.');
+    this.query('success', 'Success!', 'Link copied to clipboard.');
   }
-  
 
-
- 
 }
 
 
